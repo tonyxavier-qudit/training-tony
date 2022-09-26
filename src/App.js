ef1d7f05d4979d139289e0b1e1c7d67cab323b16
Your Name <you@example.com>
2022-09-26 17:48:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +17,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        <div>testing</div>
+      </header> */}
+        <h2 className="h2-pp">Creat Your Google Account</h2>
     </div>
+
   );
 }
 
